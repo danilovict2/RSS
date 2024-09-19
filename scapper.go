12ed3996a252aa031d4593ca6cf94eb5e6d2e7cc
danilovict2 <danilosindric@gmail.com>
2024-09-19 21:07:21 +0200

    Add function to fetch contents of a RSS feed

---
+++ scapper.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title string `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Language    string    `xml:"language"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(feedURL string) (*RSSFeed, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	response, err := client.Get(feedURL)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(body, &rssFeed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rssFeed, nil
+}
